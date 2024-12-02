a270882da9f7a50592feb1969712b8dc814d2f55
yuriy-kulakovskyi <yurakulak42@gmail.com>
2024-12-02 17:39:42 +0200

    upd: auto save order of users

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,20 @@ function App() {
 
   useEffect(() => {
     const storedUsers = localStorage.getItem("users");
-    console.log(storedUsers);
-    
-    if (storedUsers && storedUsers.length > 0) {
+    if (storedUsers) {
       setUsers(JSON.parse(storedUsers));
     } else {
       fetch("https://dummyjson.com/users?limit=10")
         .then((res) => res.json())
         .then((data) => {
           setUsers(data.users);
-          localStorage.setItem("users", JSON.stringify(data.users)); // зберігаємо користувачів у localStorage
+          localStorage.setItem("users", JSON.stringify(data.users));
         })
         .catch((error) => {
           console.error("Error fetching users:", error);
         });
     }
   }, []);
-  
 
   return (
     <div className="w-full min-h-[100vh] flex items-center justify-center">
